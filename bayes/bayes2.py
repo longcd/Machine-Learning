fe87d4dedd77cb3f40b34f194c1a8dc9e847ebe9
longcd <longcd94@163.com>
2016-12-03 23:37:33 -0800

    bayes

---
+++ bayes/bayes2.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import re
+
+def loadDataSet():
+    # 进行词条切分后的文档集合
+    postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                 ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                 ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                 ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                 ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                 ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+
+    # 类别标签的集合
+    classVec = [0, 1, 0, 1, 0, 1]    #1代表侮辱性文字，0代表正常
+    return postingList, classVec
+
+
+def createVocabList(dataSet):
+    '''
+    创建一个包含在所有文档中出现的不重复词的列表
+    '''
+
+    # 创建一个空集
+    vocabSet = set([])
+    for document in dataSet:
+        # 创建两个集合的并集
+        vocabSet = vocabSet | set(document)
+    return list(vocabSet)
+
+
+def setOfWords2Vec(vocabList, inputSet):
+    '''
+    
+    参数：
+        vocabList, 词汇表
+        inputSet, 某个文档
+
+    输出：
+        文档向量，向量的每一元素为1或0，分别表示词汇表中的单词在输入文档中是否出现。
+    '''
+    # 创建一个其中所含元素都为0的向量
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else:
+            print("the word: %s is not in my Vocabulary!" % word)
+    return returnVec
+
+
+def trainNB0(trainMatrix, trainCategory):
+    '''
+    
+    参数：
+        trainMatrix, 文档矩阵
+        trainCategory, 由每篇文档类别标签所构成的向量
+
+    步骤：
+        1. 计算文档属于侮辱性文档（class=1）的概率
+    '''
+
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    # 计算文档属于侮辱性文档（class=1）的概率
+    pAbusive = np.sum(trainCategory) / float(numTrainDocs)
+    # 初始化概率
+    p0Num = np.ones(numWords)
+    p1Num = np.ones(numWords)
+    p0Denom = 2.0
+    p1Denom = 2.0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            # 向量相加
+            p1Num += trainMatrix[i]
+            p1Denom += np.sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += np.sum(trainMatrix[i])
+    # 对每个元素做除法
+    p1Vect = np.log(p1Num / p1Denom)
+    p0Vect = np.log(p0Num / p0Denom)
+    return p0Vect, p1Vect, pAbusive
+
+
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    # 元素相乘
+    p1 = np.sum(vec2Classify * p1Vec) + np.log(pClass1)
+    p0 = np.sum(vec2Classify * p0Vec) + np.log(1.0 - pClass1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+
+def testingNB():
+    listOPosts, listClasses = loadDataSet()
+    myVocabList = createVocabList(listOPosts)
+    trainMat = []
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+    p0V, p1V, pAb = trainNB0(np.array(trainMat), np.array(listClasses))
+    testEntry = ['love', 'my', 'dalmation']
+    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
+    print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
+    testEntry = ['stupid', 'garbage']
+    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
+    print(testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb))
+
+
+def bagOfWords2VecMN(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index] += 1
+    return returnVec
